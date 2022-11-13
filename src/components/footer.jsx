7e6b82d6feb427ad53989ccb82e5d16eac55c3ef
Sid <siddhant1819@gmail.com>
2022-11-13 13:46:38 +0530

    added icons and fonts

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import './styles.css'
+import twitterlogo from '../images/iconmonstr-twitter-1.svg'
+import instalogo from '../images/iconmonstr-instagram-11.svg'
+import githublogo from '../images/iconmonstr-github-1.svg'
+
 
 const Footer = () => {
   return (
     <div className='footer'>
-        <div><a href="https://www.flaticon.com/free-icons/twitter" title="twitter icons"></a></div>
-        <div></div>
+        <div><img src={twitterlogo} /></div>
+        <div><img src={instalogo} /></div>
+        <div><img src={githublogo} /></div>
     </div>
   )
 }
--- src/components/maininfo.jsx
+++ src/components/maininfo.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './styles.css'
+import gmaillogo from '../images/iconmonstr-gmail-1.svg'
+import linkedinlogo from '../images/iconmonstr-linkedin-1.svg'
+
 const Maininfo = () => {
   return (
     <div className="maininfo">
@@ -7,8 +10,8 @@ const Maininfo = () => {
         <h4>Front End developer</h4>
         <h6>Mywebsite.something</h6>
         <div className="useful-links">
-            <div className="email">Email</div>
-            <div className="linkedin">LinkedIn</div>
+            <div className="email"><img src={gmaillogo} /><h5>Email</h5></div>
+            <div className="linkedin"><img src={linkedinlogo} /><h5>Linkedin</h5></div>
         </div>
     </div>
   )
